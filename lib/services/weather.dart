fd2770151385cd00b7d764aa37fcab0fe8373669
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-12-07 19:36:30 +0700

    Get weather from city functionality

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -5,6 +5,15 @@ const apiKey = '9f09c75ce7f7099b1e52d746062684b8';
 const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    var url = '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric';
+    NetworkHelper networkHelper = NetworkHelper(url);
+
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
   Future<dynamic> getLocationWeather() async {
     Location location = Location();
     await location.getCurrentLocation();
